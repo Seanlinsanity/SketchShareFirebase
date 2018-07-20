372d34da2ee65fc95db5e1ab2dd1f3f9a99e5885
Sean Lin <seanlin1016@gmail.com>
2018-07-20 16:30:02 +0800

    Firebase Sign In With Facebook Account

--- FirebaseFramework/Manager/FirebaseLoginManager.swift
+++ FirebaseFramework/Manager/FirebaseLoginManager.swift
@@ -8,10 +8,11 @@
 
 import Foundation
 import Firebase
+import FBSDKLoginKit
 
 //登入的部分
-public class FirebaseLogingManager
-{
+public class FirebaseLogingManager{
+    
     public init() {
         //FirebaseApp.configure()
     }
@@ -20,5 +21,26 @@ public class FirebaseLogingManager
         print(Auth.auth().currentUser?.uid ?? "No User ID")
     }
     
-    
+    public func signInFirebaseWithFB(){
+        let accessToken = FBSDKAccessToken.current()
+        guard let accessTokenString = accessToken?.tokenString else { return }
+        let credentials = FacebookAuthProvider.credential(withAccessToken: accessTokenString)
+        
+        Auth.auth().signInAndRetrieveData(with: credentials) { (result, error) in
+            if error != nil {
+                print("Failed to sign in Firebase by FB: ", error ?? "error")
+                return
+            }
+            print("Successfully logged in Firebase with facebook...")
+            print(result?.user.uid ?? "no uid")
+        }
+        
+        FBSDKGraphRequest(graphPath: "/me", parameters: ["fields": "id, name, email"]).start { (connection, result, err) in
+            if err != nil {
+                print("Failed to start graph request: ", err ?? "error")
+                return
+            }
+            print(result ?? "")
+        }
+    }
 }
--- SketchShareFirebase/ViewController.swift
+++ SketchShareFirebase/ViewController.swift
@@ -8,29 +8,48 @@
 
 import UIKit
 import FirebaseFramework
+import FBSDKLoginKit
 
 class ViewController: UIViewController {
+    
+    let customFBLoginButton: UIButton = {
+        let loginButton = UIButton(type: .system)
+        loginButton.translatesAutoresizingMaskIntoConstraints = false
+        loginButton.setTitle("Facebook帳戶登入", for: .normal)
+        loginButton.setTitleColor(.white, for: .normal)
+        loginButton.backgroundColor = UIColor(red: 66/255, green: 103/255, blue: 178/255, alpha: 1)
+        loginButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        loginButton.addTarget(self, action: #selector(handleCustomFBLogin), for: .touchUpInside)
+        return loginButton
+    }()
 
     override func viewDidLoad() {
-        
         super.viewDidLoad()
 
-//        firebaseManager.setValue(url: "testUser", value: ["name": "Sean", "email": "sean@gmail.com"])
-//        firebaseManager.deleteValue(url: "testUser")
-//        firebaseManager.pushValue(url: "test12", value: [ "id": 1234567 ])
+        setupFacebookLoginButton()
         firebaseManager.loginManager.checkUserId()
+    }
+    
+    private func setupFacebookLoginButton(){
+        view.addSubview(customFBLoginButton)
+        customFBLoginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        customFBLoginButton.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 16).isActive = true
+        customFBLoginButton.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -32).isActive = true
+        customFBLoginButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+    }
 
-//        firebaseManager.configure()
-//        firebaseManager.setValue(url: "test", obj: "test")
-
-        // Do any additional setup after loading the view, typically from a nib.
+    @objc private func handleCustomFBLogin(){
+        FBSDKLoginManager().logIn(withReadPermissions: ["email", "public_profile"], from: self) { (result, error) in
+            if error != nil{
+                print("Custom FB Login failed: ", error ?? "error")
+            }
+            firebaseManager.loginManager.signInFirebaseWithFB()
+        }
         
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
+    
+   
 
 
 }
