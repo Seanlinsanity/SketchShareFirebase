c96e0f1dba599842be648b324a7f403ff13f15cf
詹易衡 <jerryee@gapp.nthu.edu.tw>
2018-07-22 15:18:56 +0800

    Clean up ignored files

--- Pods/Target Support Files/Pods-SketchShareFirebase-FirebaseFramework/Pods-SketchShareFirebase-FirebaseFramework-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SketchShareFirebase_FirebaseFramework : NSObject
-@end
-@implementation PodsDummy_Pods_SketchShareFirebase_FirebaseFramework
-@end
--- Pods/Target Support Files/Pods-SketchShareFirebase/Pods-SketchShareFirebase-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SketchShareFirebase : NSObject
-@end
-@implementation PodsDummy_Pods_SketchShareFirebase
-@end
